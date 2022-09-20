98c1dca25256e55d23278ee3a05c751dacf9cac4
Darshan Patil <iampatildarsh>
2022-09-20 11:21:30 +0530

    firstJava file added

---
+++ firstJava.java
@@ -0,0 +1,8 @@
+class firstjava {
+public static void main(String[] args) {
+		(System.out.println("hello world.... from master");
+		(System.out.println("hello world.... from test demo");
+	
+	}
+}
+
