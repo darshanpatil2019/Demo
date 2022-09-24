72f5d9393fdea97342e2d0878a2d4b14643487b1
Darshan Patil <iampatildarsh>
2022-09-24 12:31:22 +0530

    test file uploaded

---
+++ test.sh
@@ -0,0 +1,29 @@
+# Problem 1
+int main {
+int year ;
+echo ("Enter a Year ");
+echo ("Enter a Number" , &year);
+
+// leap year if perfectly divisible by 400
+if (year % 400==0) {
+echo $("%d is a leap year.", year);
+}
+// leap year if perfectly divisible by 100
+// but not divisible by 400
+else if (year % 100==0) {
+echo $("%d is not a leap year.", year);
+}
+// leap year if perfectly divisible by 100
+but divisible by 4
+else if (year % 4==0) {
+echo $("%d is a leap year.", year);
+}
+// all other years are not leap years
+else {
+ {
+echo $("%d is not a leap year.", year);
+}
+
+
+
+
